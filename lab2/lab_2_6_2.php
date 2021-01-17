38f396b252d9c781a6372367ccdc1dd0429515c7
edgarbee <57279210+edgarbee@users.noreply.github.com>
2021-01-18 04:12:53 +0500

    Изменен вариант

--- lab2/lab_2_6_2.php
+++ lab2/lab_2_6_2.php
@@ -4,43 +4,26 @@
 	<title>Razyapov Edgar</title>
 </head>
 <body>
-	<img src="2.png">
-
+	<h1>Вариант 13</h1>
 <?php
-echo '<br/>';
-$a = rand(-20,20);
-$b = rand(-20,20);
+    function F($u, $t){
+        if ($u*$t < 0.5){
+            return (1 + cos($t-$u))/($u/$t + $t*$t);
+        }
+        else if($u*$t >= 0.5){
+            return sin(log(abs($u/$t)));
+        }
+    }
 
-echo 'Первое число: ' . $a . '<br/>';
-echo 'Второе число: ' . $b;
+    $a = rand(-10, 10);
+    $b = rand(-10, 10);
 
-// вторая часть первого условия
-$t1 = $a*$a;
-$t2 = $a + $b;
-$F = array(2);
+    echo "<p> Аргумент A = ".$a;
+    echo "<p> Аргумент B = ".$b;
 
-for ($i=0; $i < 2; $i++) {
-	//1
-if ($a < -2) {
-	$F[$i] = 2*$a;
-} elseif ($t1 < -2) {
-	$F[$i] = 2*$t1;
-}
-	//2
-if ($a>-2 && $a < 2) {
-		$F[$i] = pow(sin($a), 3) + log($b);
-	} elseif ($t1 > -2 && $t1 < 2) {
-	$F[$i] = pow(sin($t1), 3) + log($t2);
-}
-	//3
-if ($a> 2) {
-		$F[$i] = pow(pow(cos($a), 2) + pow(sin($b*$b*$b), 4), 1/4);
-	} elseif ($t1> 2) {
-	$F[$i] = pow(pow(cos($t1), 2) + pow(sin($t2*$t2*$t2), 4), 1/4);
-}
-}
-$z = $F[0] + $F[1]; // Сумма функций
-echo '<br/>' . 'Сумма функций равна: ' . $z;
+    $z = pow(cos(F($a, $b * $b - $a)), 3) + F($a+$b,$a-$b);
+
+    echo "<p> Результат = ".$z;
 ?>
 </body>
 </html>
