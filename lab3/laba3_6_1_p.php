037dcb6d668f8103ec723a5a820b965d04b45d25
edgarbee <57279210+edgarbee@users.noreply.github.com>
2021-01-18 04:14:16 +0500

    Изменен вариант

--- lab3/laba3_6_1_p.php
+++ lab3/laba3_6_1_p.php
@@ -1,23 +1,9 @@
 <?php
-$TEXT = $_POST["givenText"];
+$text = $_POST["givenText"];
 
- $count = strlen(utf8_decode($TEXT));
- $mas = str_split($TEXT);
-
- $k = 0;
- $n = 0;
- for ($i=0; $i < $count ; $i++) {
-   if (strtoupper($mas[$i]) == $mas[$i]) {
-     $k = $k + 1;
-   } else {
-     $n = $n + 1;
-   }
- }
- echo 'Общее число заглавных букв: ' . $k . '<br>';
- echo 'Общее число прописных букв: ' . $n . '<br>';
- echo 'Общее число символов в тексте: ' . $count . '<br>';
- echo 'Общее число символов в процентах: ' . ($count/$count)*100 .'%'. '<br>';
- echo '<b>Ответ:</b><br>';
- echo 'Заглавных букв: ' . ($k/$count)*100 .'%'. '<br>';
- echo 'Прописных букв: ' . ($n/$count)*100 .'%' .'<br>';
+$arr = explode(" ", $text);
+ 
+foreach ($arr as $val) {
+     echo $val . "<br>";
+}
 ?>
