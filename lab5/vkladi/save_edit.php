186215a4a86fd9c599385dcdc04bcb63f76696b7
edgarbee <57279210+edgarbee@users.noreply.github.com>
2021-01-18 04:20:05 +0500

    Корректное сохранение изменений

--- lab5/vkladi/save_edit.php
+++ lab5/vkladi/save_edit.php
@@ -6,11 +6,11 @@ $mysqli = new mysqli("localhost","f0505640_edgar","1234","f0505640_edgar");
 
     $id = $_GET['id'];
     $date= $_GET['date'];
-    $dep_id= $_GET['dep_id'];
+    $id_dep=$_GET['id_dep'];
     $st_cont=$_GET['st_cont'];
 
     $result = $mysqli->query("UPDATE cont
-        SET date='$date', id_dep='$dep_id', st_cont='$st_cont'
+        SET date='$date', id_dep='$id_dep', st_cont='$st_cont'
         WHERE id='$id'"
     );
 
